b55b7900e85fda60a811b77f7963fb8b113484c0
kanainjapan <66121506+kanainjapan@users.noreply.github.com>
2020-07-21 23:38:50 +0900

    Lots of Changes

---
+++ fonts.js
@@ -0,0 +1,5 @@
+/*var link = document.createElement('link');
+link.setAttribute('rel', 'stylsheet');
+linke.setAttribute{'type', 'text/css');
+link.setAttribute('href', 'https://fonts.googleapis.com/css2?family=Beth+Ellen&family=Finger+Paint&family=Londrina+Sketch&display=swap');
+document.head.appendChild(link);*/
